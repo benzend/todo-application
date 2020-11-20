6fa3e25841fbcb5518cccf421388b39d96458207
Benjamin <1234hteimaj4321@gmail.com>
2020-11-20 13:36:24 -0700

    added checked hook and finished hook

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,11 +1,17 @@
 import { ListItem, Checkbox, IconButton } from "@material-ui/core";
 import DeleteIcon from "@material-ui/icons/Delete";
 
-export default function Todo({ text }) {
+export default function Todo(props) {
+  const finishedStyle = {
+    textDecoration: "line-through",
+    opacity: 0.5,
+  };
   return (
-    <ListItem>
-      <Checkbox />
-      <p>{text}</p>
+    <ListItem style={{ justifyContent: "space-between" }}>
+      <div style={props.finished ? finishedStyle : null}>
+        <Checkbox checked={props.checked} />
+        {props.text}
+      </div>
       <IconButton>
         <DeleteIcon />
       </IconButton>
--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,14 +1,28 @@
+import { useState } from "react";
+
 import { List, Card } from "@material-ui/core";
 import Todo from "./Todo";
 
 export default function Todos() {
+  const [finished, setFinished] = useState([]);
+  const [checked, setChecked] = useState([false, true]);
   return (
     <div>
       <h2>Your Todo List</h2>
       <Card style={{ width: "400px", margin: "auto" }}>
         <List>
-          <Todo text="Go get food" />
-          <Todo text="Go get monies" />
+          <Todo
+            checked={checked[0]}
+            finished={finished[0]}
+            setFinished={setFinished}
+            text="Go get food"
+          />
+          <Todo
+            checked={checked[1]}
+            finished={finished[1]}
+            setFinished={setFinished}
+            text="Go get monies"
+          />
         </List>
       </Card>
     </div>
