e854568b2c6bd80cb91a495e1af8c3f925270425
Benjamin <1234hteimaj4321@gmail.com>
2020-11-20 12:56:18 -0700

    merged with main

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,10 +1,12 @@
 import { ListItem, Checkbox } from "@material-ui/core";
+import DeleteIcon from "@material-ui/icons/Delete";
 
 export default function Todo({ text }) {
   return (
     <ListItem>
       <Checkbox />
       {text}
+      <DeleteIcon />
     </ListItem>
   );
 }
