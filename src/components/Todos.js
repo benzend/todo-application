87c4177d84916acfda65ef45a4e07462decc9792
Benjamin <1234hteimaj4321@gmail.com>
2020-11-20 13:52:08 -0700

    added mapping for each todo item

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -6,6 +6,11 @@ import Todo from "./Todo";
 export default function Todos() {
   const [finished, setFinished] = useState([]);
 
+  const todos = [
+    { id: 1, text: "Go get foods" },
+    { id: 2, text: "Go get monies" },
+  ];
+
   const checkboxHandler = (e) => {
     if (e.target.id == 1) {
       console.log("this is the first item");
@@ -29,18 +34,16 @@ export default function Todos() {
       <h2>Your Todo List</h2>
       <Card style={{ width: "400px", margin: "auto" }}>
         <List>
-          <Todo
-            id="1"
-            checkboxHandler={checkboxHandler}
-            finished={finished[0]}
-            text="Go get food"
-          />
-          <Todo
-            id="2"
-            checkboxHandler={checkboxHandler}
-            finished={finished[1]}
-            text="Go get monies"
-          />
+          {todos.map((todo, index) => {
+            return (
+              <Todo
+                id={todo.id}
+                checkboxHandler={checkboxHandler}
+                finished={finished[index]}
+                text={todo.text}
+              />
+            );
+          })}
         </List>
       </Card>
     </div>
