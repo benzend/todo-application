e86fd02f69f1404b3c3c796461b8cefdac2a7ebb
Benjamin <1234hteimaj4321@gmail.com>
2020-11-20 13:19:46 -0700

    added IconButton

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,12 +1,14 @@
-import { ListItem, Checkbox } from "@material-ui/core";
+import { ListItem, Checkbox, IconButton } from "@material-ui/core";
 import DeleteIcon from "@material-ui/icons/Delete";
 
 export default function Todo({ text }) {
   return (
     <ListItem>
       <Checkbox />
-      {text}
-      <DeleteIcon />
+      <p>{text}</p>
+      <IconButton>
+        <DeleteIcon />
+      </IconButton>
     </ListItem>
   );
 }
