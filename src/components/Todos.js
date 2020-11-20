7ad79ed7761b48c76090ad160481b69ecd325ff4
Benjamin <1234hteimaj4321@gmail.com>
2020-11-20 12:51:26 -0700

    added basic setup

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,14 +1,16 @@
-import { List } from "@material-ui/core";
+import { List, Card } from "@material-ui/core";
 import Todo from "./Todo";
 
 export default function Todos() {
   return (
     <div>
       <h2>Your Todo List</h2>
-      <List>
-        <Todo text="Go get food" />
-        <Todo text="Go get monies" />
-      </List>
+      <Card style={{ width: "400px", margin: "auto" }}>
+        <List>
+          <Todo text="Go get food" />
+          <Todo text="Go get monies" />
+        </List>
+      </Card>
     </div>
   );
 }
